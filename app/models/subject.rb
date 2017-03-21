40044d75a47f66b69f9b06e4b78eafe8c348c458
huxtim <huxtim@gmail.com>
2017-03-21 14:28:27 -0400

    fixed db association issue

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,5 @@
 class Subject < ApplicationRecord
-  has_many :relationships, foreign_key: "subject_indicator",
-                           primary_key: "indicator",
-                           dependent:   :destroy
+  has_many :relationships, dependent:   :destroy
   has_many :courses, :through => :relationships
-  has_many :courses, :through => :relationships
+  self.primary_key = "indicator"
 end
